0885b8e155621a9d40ad7b67da11a3b61c42fc3c
Julian <juliangolaviaga@gmail.com>
2026-07-21 19:10:10 -0300

    Actualizar core_crear_tablas.sql

--- techba-rrhh/Script/core_vistas.sql
+++ techba-rrhh/Script/core_vistas.sql
@@ -1,12 +1,13 @@
 ----------------------Asuentismo por empleados-----------------------
-CREATE VIEW core.ausencias_x_empleado AS
+CREATE OR REPLACE VIEW  core.ausencias_x_empleado AS
 Select
 	--a.AUSENCIA_ID,
 	--a.EMPLEADO_ID,
 	e.APELLIDO || ', ' || e.NOMBRE AS Empleado,
 	a.TIPO AS Tipo_Ausencia,
 	a.justificado as Justificado,
-	lower(to_char(a.FECHA_INICIO, 'DD-Mon-YYYY')) || ' - ' || lower(to_char(a.FECHA_FIN, 'DD-Mon-YYYY')) AS Periodo
+	lower(to_char(a.FECHA_INICIO, 'DD-Mon-YYYY')) || ' - ' || lower(to_char(a.FECHA_FIN, 'DD-Mon-YYYY')) AS Periodo,
+	a.FECHA_FIN - a.FECHA_INICIO AS Dias_Periodo	
 FROM core.ausentismo a
 JOIN core.EMPLEADOS e on e.EMPLEADO_ID = a.EMPLEADO_ID
 GROUP BY
@@ -15,46 +16,40 @@ GROUP BY
 	Empleado,
 	a.TIPO,
 	a.justificado,
-	lower(to_char(a.FECHA_INICIO, 'DD-Mon-YYYY')) || ' - ' || lower(to_char(a.FECHA_FIN, 'DD-Mon-YYYY'))
+	lower(to_char(a.FECHA_INICIO, 'DD-Mon-YYYY')) || ' - ' || lower(to_char(a.FECHA_FIN, 'DD-Mon-YYYY')),
+	a.FECHA_FIN - a.FECHA_INICIO
 ORDER BY Empleado
 ---------------------------------------------------------------------
 
---ANLITICAS
+----------------------Evaluaciones por empleados-----------------------
+CREATE OR REPLACE VIEW  core.valuaciones_x_empleado AS
+select
+	em.empleado_id,
+	em.APELLIDO || ', ' || em.NOMBRE AS Empleado,
+	ev.periodo_original as Periodo,
+	ev.puntaje as Puntaje,
+	ev.categoria as Categoria
+	--e.APELLIDO || ', ' || e.NOMBRE AS Empleado,
+FROM core.evaluaciones ev
+JOIN core.empleados em on em.empleado_id = ev.empleado_id
+GROUP by
+	em.empleado_id,
+	Empleado,
+	Periodo,
+	Puntaje,
+	Categoria
+ORDER BY Empleado
 
---Cálculo de días de ausencia
-    --Solo si ambas fechas pudieron ser rescatadas y limpiadas correctamente
-    CASE 
-        WHEN utils.limpiar_fecha_generica(a.fecha_fin) IS NOT NULL 
-         AND utils.limpiar_fecha_generica(a.fecha_inicio) IS NOT NULL
-        THEN 
-            -- Si la fecha fin es menor, calculamos la diferencia invertida (rescatamos el dato)
-            CASE 
-                WHEN utils.limpiar_fecha_generica(a.fecha_fin)::DATE < utils.limpiar_fecha_generica(a.fecha_inicio)::DATE 
-                THEN (utils.limpiar_fecha_generica(a.fecha_inicio)::DATE - utils.limpiar_fecha_generica(a.fecha_fin)::DATE) + 1
-                ELSE (utils.limpiar_fecha_generica(a.fecha_fin)::DATE - utils.limpiar_fecha_generica(a.fecha_inicio)::DATE) + 1
-            END
-        ELSE NULL 
-    END AS dias_duracion_ausencia,
 
-    -- COLUMNA DE ALERTA: Cantidad de dias de ausencia qeu daban negativos, 
-    --se invierten las fechas para el calculo y se deja constancia (True=fecha invertida)
-    CASE 
-        WHEN utils.limpiar_fecha_generica(a.fecha_fin)::DATE < utils.limpiar_fecha_generica(a.fecha_inicio)::DATE 
-        THEN TRUE ELSE FALSE 
-    END AS alerta_fechas_invertidas
-    
-    
-    
-    
-    -- 3. NORMALIZACIÓN FINANCIERA (Convertimos todo a Pesos Argentinos para poder sumar y promediar)
-    CASE 
-        WHEN utils.limpiar_espacios(p.moneda) = 'USD' THEN utils.limpiar_numero(p.salario_min)::NUMERIC * 1000.0 -- Cotización ejemplo
-        ELSE utils.limpiar_numero(p.salario_min)::NUMERIC 
-    END::NUMERIC(12,2) AS salario_min_ars,
+SELECT email, COUNT(DISTINCT empleado_id) AS cant_ids
+FROM core.empleados
+GROUP BY email
+HAVING COUNT(DISTINCT empleado_id) > 1
+ORDER BY cant_ids DESC;
 
-    CASE 
-        WHEN utils.limpiar_espacios(p.moneda) = 'USD' THEN utils.limpiar_numero(p.salario_max)::NUMERIC * 1000.0 -- Cotización ejemplo
-        ELSE utils.limpiar_numero(p.salario_max)::NUMERIC 
-    END::NUMERIC(12,2) AS salario_max_ars
-    
-    
+SELECT empleado_id, nombre, apellido, telefono, fecha_nacimiento, fecha_ingreso, depto_id
+FROM core.empleados
+WHERE email IN ('aldana.rojas@techba.com','camila.molina@techba.com','cecilia.cabrera@techba.com',
+'cecilia.medina@techba.com','cecilia.nunez@techba.com','ezequiel.gomez@techba.com',
+'micaela.herrera@techba.com','natalia.molina@techba.com','ramiro.vera@techba.com')
+ORDER BY email, empleado_id;
